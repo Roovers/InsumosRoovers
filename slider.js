9fe64f2bb9e9f7efc204869d5f80bc465980a36e
Roovers <91910203+Roovers@users.noreply.github.com>
2025-07-01 22:55:19 -0300

    Add files via upload

--- slider.js
+++ slider.js
@@ -13,14 +13,3 @@ const swiper = new Swiper('.swiper', {
     disableOnInteraction: false,
   },
 });
-
-// Hamburguesa toggle
-document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.getElementById("menu-toggle");
-  const menu = document.querySelector(".menu");
-
-  toggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
-  });
-});
-
